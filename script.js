3a92a6aaaade04311fa6e488f99cd4fbd9802427
iamsaief <saiefalemon@gmail.com>
2020-07-23 20:00:36 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+const productPrices = [1219, 59];
+let subtotalPrice = 0;
+let subtotalArr = [...productPrices];
+
+// Event Bubbling to capture both the cart-items
+document
+	.querySelectorAll(".cart-item:not(.cart-checkout)")
+	.forEach((item, index) => {
+		item.addEventListener("click", (e) => {
+			const { target, currentTarget } = e;
+			const price = currentTarget.querySelector(".item-price");
+			const unit = currentTarget.querySelector(".item-unit");
+
+			if (
+				target.classList.contains("btn-plus") ||
+				target.classList.contains("fa-plus")
+			) {
+				updatedPrice(unit, price, productPrices[index], "+", index);
+			}
+			if (
+				target.classList.contains("btn-minus") ||
+				target.classList.contains("fa-minus")
+			) {
+				updatedPrice(unit, price, productPrices[index], "-", index);
+			}
+			if (target.classList.contains("remove-item")) {
+				console.log(item);
+
+				if (
+					window.confirm(
+						`Are you sure ? 😲 Press Cancel to leave it, OK to remove.`
+					)
+				) {
+					item.style.display = "none";
+
+					updatedPrice(unit, price, productPrices[index], "-", index);
+				}
+			}
+
+			console.log(target);
+		});
+	});
+
+// Cart Item and Price Calculation
+function updatedPrice(unit, priceEl, basePrice, status, index) {
+	let newUnit;
+	if (status === "+") {
+		newUnit = ++unit.value;
+	} else {
+		newUnit = --unit.value;
+	}
+
+	let newPrice = basePrice * newUnit;
+	priceEl.innerText = newPrice;
+
+	subtotalArr[index] = newPrice;
+	checkoutPrice(subtotalArr);
+}
+
+// Cart Checkout Calculation (assuming 10% tax)
+function checkoutPrice(subtotalArr) {
+	let [a, b] = subtotalArr;
+	const subtotal = document.querySelector(".subtotal");
+	const tax = document.querySelector(".tax");
+	const total = document.querySelector(".total");
+	subtotalPrice = a + b;
+	const taxPrice = parseFloat((subtotalPrice * 0.1).toFixed(2));
+	const totalPrice = Math.ceil(subtotalPrice + taxPrice);
+
+	subtotal.innerText = 0;
+	tax.innerText = 0;
+	total.innerText = 0;
+
+	subtotal.innerText = subtotalPrice;
+	tax.innerText = taxPrice;
+	total.innerText = totalPrice;
+}
+
+const checkoutBtn = document
+	.querySelector(".check-out")
+	.addEventListener("click", () => {
+		const cartContent = document.querySelector("[class^=col-]");
+		const greetings = document.querySelector(".greetings");
+		cartContent.style.display = "none";
+		greetings.style.display = "block";
+	});
